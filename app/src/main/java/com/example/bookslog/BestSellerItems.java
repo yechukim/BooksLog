e5fcad900c7315355187963893711af91e228deb
yechubot <yechukim217@gmail.com>
2020-11-18 14:15:48 +0900

    best item added

---
+++ app/src/main/java/com/example/bookslog/BestSellerItems.java
@@ -0,0 +1,38 @@
+package com.example.bookslog;
+
+public class BestSellerItems {
+    String title;
+    String author;
+    String category;
+
+    public BestSellerItems(String title, String author, String category) {
+        this.title = title;
+        this.author = author;
+        this.category = category;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+}
--- app/src/main/java/com/example/bookslog/Frag_best.java
+++ app/src/main/java/com/example/bookslog/Frag_best.java
@@ -2,6 +2,7 @@ package com.example.bookslog;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -11,13 +12,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Frag_best extends Fragment {
     Button btn_goSite;
     Intent siteIntent;
+    GridView gridView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,6 +34,7 @@ public class Frag_best extends Fragment {
         getActivity().setTitle("베스트셀러");
         View fragView = inflater.inflate(R.layout.fragment_frag_best, container, false);
         btn_goSite = fragView.findViewById(R.id.btn_goSite);
+        gridView = fragView.findViewById(R.id.gridView);
         btn_goSite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -34,8 +43,50 @@ public class Frag_best extends Fragment {
                 startActivity(siteIntent);
             }
         });
+
         return  fragView;
     }
 
+    class GridAdapter extends BaseAdapter{
+        Context context;
+        LayoutInflater layoutInflater;
+        List<BestSellerItems> bestSellerLists = new ArrayList<>();
+
+        public GridAdapter(Context c, List<BestSellerItems> bestSellerItems) {
+            this.context = c;
+            this.layoutInflater = LayoutInflater.from(context);
+            this.bestSellerLists = bestSellerItems;
+        }
+
+        @Override
+        public int getCount() {
+            return bestSellerLists.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return bestSellerLists.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = convertView;
+            if(view==null){
+                view = layoutInflater.inflate(R.layout.best_items,null);
+            }
+            ImageView bestItemCover = view.findViewById(R.id.bestItemCover);
+            TextView bestItemAuthor = view.findViewById(R.id.bestItemAuthor);
+            TextView bestItemTitle = view.findViewById(R.id.bestItemTitle);
+
+            BestSellerItems bestItems = bestSellerLists.get(position);
+
+            return null;
+        }
+    }
 
 }
